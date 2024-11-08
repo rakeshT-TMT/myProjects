f715a36c7d086e6d32babb640e7c3cfec4af1a5a
rakeshT-TMT <thotarakesh2001@gamial.com>
2024-11-08 09:41:38 +0530

    init commit

--- com/example/ThreadsCodes/Synchronized.java
+++ com/example/ThreadsCodes/Synchronized.java
@@ -5,12 +5,12 @@ public class Synchronized {
         BrickDiary bd=new BrickDiary();
 
         Runnable r1=()->{
-            for(int i=0;i<10000;i+=50){
+            for(int i=0;i<30000;i+=50){
                 bd.incrementBrickCount();
             }
         };
         Runnable r2=()->{
-            for(int i=0;i<25000;i+=50){
+            for(int i=0;i<20000;i+=50){
                 bd.incrementBrickCount();
             }
         };
@@ -32,16 +32,51 @@ public class Synchronized {
         t2.join();
         t3.join();
 
-        System.out.println(bd.brickCount);
-        System.out.println(bd.brickCount2);
+//        System.out.println(bd.brickCount);
+//        System.out.println(bd.brickCount2);
+        System.out.println(bd.getBrickCount());
+        System.out.println(bd.getBrickCount2());
 
     }
 }
-class BrickDiary{
-    int brickCount=0;
-    int brickCount2=0;
-    public synchronized void incrementBrickCount(){
-        brickCount+=50;
-        brickCount2+=50;
+//class BrickDiary{
+//    int brickCount=0;
+//    int brickCount2=0;
+//    public synchronized void incrementBrickCount(){
+//
+//            brickCount += 50;
+//            brickCount2 += 50;
+//    }
+//
+//}
+class BrickDiary {
+    private int brickCount = 0;
+    private int brickCount2 = 0;
+    private final int LIMIT = 500;
+
+    public synchronized void incrementBrickCount() {
+        try {
+            while (brickCount >= LIMIT) {
+
+                wait();
+            }
+            brickCount += 50;
+            brickCount2 += 50;
+            System.out.println(Thread.currentThread().getName() + " incremented: brickCount = " + brickCount + ", brickCount2 = " + brickCount2);
+
+
+            notify();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println(Thread.currentThread().getName() + " was interrupted.");
+        }
+    }
+
+    public synchronized int getBrickCount() {
+        return brickCount;
+    }
+
+    public synchronized int getBrickCount2() {
+        return brickCount2;
     }
 }
